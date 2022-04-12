288caedb14911c8c0441ca0c6421c58b778b6d4e
Remi Etchebarne <retcheba@3C3.42angouleme.fr>
2022-04-12 19:00:45 +0200

    +split -> libft

--- 42cursus/libft/libft.h
+++ 42cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: retcheba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 18:08:42 by retcheba          #+#    #+#             */
-/*   Updated: 2022/04/10 10:08:15 by retcheba         ###   ########.fr       */
+/*   Updated: 2022/04/12 14:48:45 by retcheba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
